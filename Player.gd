ed876ee7c16fb0e6575311dbc4165027ef5af396
Piotr Durniat <piotr@durniat.com>
2022-05-25 19:36:56 +0200

    Changed movement mechanic

--- Player.gd
+++ Player.gd
@@ -5,38 +5,58 @@ signal hit
 export var speed = 400 # player speed in px/sec
 var screen_size # game window size
 
+var velocity = Vector2.ZERO # the players movement vector
+var max_speed = 500
+var direction = PI/2
+var rotation_speed = PI/18
+var attenuation = 0.98
+
 func _ready():
 	screen_size = get_viewport_rect().size
+	$AnimatedSprite.animation = "up"
 	hide()
 	
 func _process(delta):
-	var velocity = Vector2.ZERO # the players movement vector
+	var acceleration = Vector2.ZERO # the players movement vector
+	
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		direction += rotation_speed
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		direction -= rotation_speed
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		pass
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		acceleration.y -= 20
+		acceleration = acceleration.rotated(direction)
 		 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
-	else:
+	velocity += acceleration
+	velocity = velocity.clamped(max_speed)
+	velocity *= attenuation
+	
+	if Input.is_action_just_released("move_up"):
 		$AnimatedSprite.stop()
-	
+	elif Input.is_action_just_pressed("move_up"):
+		$AnimatedSprite.play()
+		
 	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
+
+	if position.x > screen_size.x:
+		position.x -= screen_size.x
+	elif position.x < 0:
+		position.x += screen_size.x
+			
+	if position.y > screen_size.y:
+		position.y -= screen_size.y
+	elif position.y < 0:
+		position.y += screen_size.y
 	
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "walk"
-		$AnimatedSprite.flip_v = false
-		$AnimatedSprite.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite.animation = "up"
-		$AnimatedSprite.flip_v = velocity.y > 0
+	if rotation > PI * 2:
+		rotation -= PI * 2
+	elif rotation < -PI * 2:
+		rotation += PI * 2
+	
+	$AnimatedSprite.rotation = direction
+	
 
 
 func _on_Player_body_entered(body):
